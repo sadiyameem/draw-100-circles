e055350ff62bfc3388c5015386b7499322407903
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-11-30 11:22:40 -0500

    change the brightness

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ t =0
 function a() {
     t += 1
     if(t % 20 == 0 && t <= 800) {
-        h = document.createElement("div")
+        let h = document.createElement("div")
         h.style.aspectRatio = '1/1'
         h.style.height = `${(Math.random()*60+50)}px`
         h.style.position = "fixed"
         h.className = "h"
-        h.style.filter = `brightness(${0.5+Math.random()*0.5})`
+        h.style.filter = `brightness(${0.8+Math.random()*0.2})`
         h.style.left = `${Math.random() * window.innerWidth}px`
         h.style.top = `${Math.random() * window.innerHeight}px`
         h.style.backgroundColor = "cornflowerblue"
