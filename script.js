7bb1e16ac2ad3ddbb8f4a26e9f215a2323c62f07
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-11-30 10:58:59 -0500

    added css and now fixing things so it stays as a circle when hovered

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function a() {
     t += 1
     if(t % 20 == 0 && t <= 800) {
         h = document.createElement("div")
-        h.style.width = "60px"
-        h.style.height = "60px"
-        h.style.position = "absolute"
+        h.style.aspectRatio = '1/1'
+        h.style.height = `${(Math.random()*60+50)}px`
+        h.style.position = "fixed"
+        h.className = "h"
         h.style.left = `${Math.random() * window.innerWidth}px`
         h.style.top = `${Math.random() * window.innerHeight}px`
         h.style.backgroundColor = "cornflowerblue"
         h.style.borderRadius = "100%"
+        h.style.boxShadow = "0 0 25px cornflowerblue"
         document.body.appendChild(h) 
     }
 }
