1b9518fd4e81688c131dc31c5ea5703520def286
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-12-01 19:30:58 -0500

    change the order of the code and add a section where the circles can go

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ function a() {
 
     // only create a circle every 20 intervals (repeated time gap), stop after 800 intervals
     if(t % 20 == 0 && t <= 800) {
-        let h = document.createElement("div")
 
-        // make the div a perfect square
-        h.style.aspectRatio = '1/1'
+        let bowl = document.getElementById("bowl");
+        let rect = bowl.getBoundingClientRect();
 
-        // random height between 50px and 110px
-        h.style.height = `${(Math.random()*60+50)}px`
+        let h = document.createElement("div"); // make new circle
+        h.style.aspectRatio = '1/1' // make the div a perfect square
+        h.style.height = `${(Math.random()*60+50)}px` // random height between 50px and 110px
+        h.style.position = "absolute" // position the circles on the screen
+        h.className = "h" // class name for css
 
-        // position the circles on the screen
-        h.style.position = "fixed"
-        h.style.left = `${Math.random() * window.innerWidth}px`
-        h.style.top = `${Math.random() * window.innerHeight}px`
-
-        // class name for css
-        h.className = "h"
+        // random position inside the bowl
+        h.style.left = `${Math.random() * rect.width}px`;
+        h.style.top = `${Math.random() * rect.height}px`;
 
         // base color for brightness
         h.style.filter = `brightness(${0.8+Math.random()*0.2})`
@@ -30,10 +28,10 @@ function a() {
         h.style.borderRadius = "100%"
 
         // add a glow/shade using the same color
-        h.style.boxShadow = "0 0 25px cornflowerblue"
+        h.style.boxShadow = "0 0 25px cornflowerblue";
 
         // add the circles to the page
-        document.body.appendChild(h) 
+        document.getElementById("bowl").appendChild(h) 
     }
 }
 
